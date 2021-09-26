a62751187155b1d6ec03ec66f144b1100ef5bcc4
ccarver80 <ckc_80@outlook.com>
2021-09-26 15:19:36 -0600

    fixed bugs

--- js/app.js
+++ js/app.js
@@ -4,29 +4,24 @@
 const startButton = document.getElementById("btn__reset");
 let game; //declare a game varible to use in a global scope outside of my startButton event listener
 
-
 // Creates a new Game object when start button is pressed
-startButton.addEventListener('click', () => {
-    game = new Game();
-    game.startGame(); 
-})
+startButton.addEventListener("click", () => {
+  game = new Game();
+  game.startGame();
+});
 
-
-// adds an event listener to each keyboard letter and calls the handleInteraction method and passes in the letter pressed 
+// adds an event listener to each keyboard letter and calls the handleInteraction method and passes in the letter pressed
 for (let i = 0; i < keyLetter.length; i++) {
-    keyLetter[i].addEventListener('click', (e) => {
-        game.handleInteraction(keyLetter[i])
-        
-    })
+  keyLetter[i].addEventListener("click", (e) => {
+    game.handleInteraction(keyLetter[i]);
+  });
 }
 
 //Keyboard event listener to check each key letter for the corresponding onscreen keyletter and handling the interaction
-document.addEventListener('keyup', (e) => {
-    for (let i = 0; i < keyLetter.length; i++) {
-        if (e.key === keyLetter[i].innerHTML) {
-            game.handleInteraction(keyLetter[i])
-        }
-    }  
-})
-
-
+document.addEventListener("keyup", (e) => {
+  for (let i = 0; i < keyLetter.length; i++) {
+    if (e.key === keyLetter[i].innerHTML) {
+      game.handleInteraction(keyLetter[i]);
+    }
+  }
+});
